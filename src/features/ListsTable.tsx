2f08e3abe5cffc130d6ba54ae89e6cd81babf254
Tae Kim <tae.kim@airbnb.com>
2020-04-26 18:58:27 -0500

    Display the complete/incomplete messages
    User’s now have an indication of how many tasks are complete/incomplete in their List. We re-use all of the Task data that is on our Global State. EZ PZ.

--- src/features/ListsTable.tsx
+++ src/features/ListsTable.tsx
@@ -11,8 +11,38 @@ import styles from './ListsTable.module.scss';
 const Lists = () => {
   const { lists } = React.useContext(GlobalStateContext);
 
+  // Returns the number of completed and incompleted Tasks in a List.
+  const getCompletedAndIncompleted = (listId: number) => {
+    const listIndex = lists.findIndex(({ id }) => id === listId);
+    const list = lists[listIndex];
+    const completedTasks = list.tasks.filter(({ complete }) => complete);
+
+    const completed = completedTasks.length;
+    const incompleted = list.tasks.length - completed;
+
+    return [completed, incompleted];
+  };
+
+  // Returns the description to display for a given list.
+  const getCompletedDescription = (listId: number) => {
+    const [completed, incompleted] = getCompletedAndIncompleted(listId);
+    if (completed === 0 && incompleted === 0) {
+      return 'There are no Tasks in this list.';
+    }
+
+    if (incompleted === 0) {
+      return `${completed} complete.`;
+    }
+
+    if (completed === 0) {
+      return `${incompleted} incomplete.`;
+    }
+
+    return `${completed} complete, ${incompleted} incomplete.`;
+  };
+
   return (
-    <div>
+    <div className={styles.container}>
       {lists.length === 0 && <div>There are no lists. Please create one.</div>}
 
       {lists.length > 0 && (
@@ -25,15 +55,18 @@ const Lists = () => {
               <b>Progress</b>
             </div>
           </div>
-          {lists.map(({ id, name }) => (
-            <div key={id} className={styles.row}>
-              <div className={styles.column}>
-                <Link to={`/lists/${id}`}>{name}</Link>
+          {lists.map(({ id, name }) => {
+            return (
+              <div key={id} className={styles.row}>
+                <div className={styles.column}>
+                  <Link to={`/lists/${id}`}>{name}</Link>
+                </div>
+                <div className={styles.column}>
+                  {getCompletedDescription(id)}
+                </div>
               </div>
-              {/* TODO: Get the task completion */}
-              <div className={styles.column}>1 complete, 1 incomplete</div>
-            </div>
-          ))}
+            );
+          })}
         </>
       )}
     </div>
